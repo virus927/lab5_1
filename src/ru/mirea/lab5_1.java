2d80f2ce10e526ca3fdb718ecc7dbfbdb44b93e5
Tohir <krogstat@gmail.com>
2020-09-28 14:03:58 +0300

    Initial commit

---
+++ src/ru/mirea/lab5_1.java
@@ -0,0 +1,10 @@
+package ru.mirea;
+
+public class lab5_1 {
+    public static void main(String[] args) {
+        double[] qwe = {1, 2, 3, 4};
+        for (int i = qwe.length - 1; i >= 0; i--) {
+            System.out.print(qwe[i] + " ");
+        }
+    }
+}
